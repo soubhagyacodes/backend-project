2027c50e3dee039c029d305d20041949f1c5924a
soubhagyacodes <soubhagya613mishra@gmail.com>
2025-03-18 22:52:18 +0530

    Code Enhancement Utils

--- src/app.js
+++ src/app.js
@@ -3,7 +3,16 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express()
-app.use(cors())
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "18kb"}))
+app.use(express.urlencoded({extended:true, limit: "18kb"}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
 
 export default app
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import { DBconnect } from "./db/index.js";
 import app from './app.js';
 
 DBconnect()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server Listening on port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Fault!", err)
+})
 
 app.get('/', (req, res) => {
     res.send("HELLO WORLD!")
-})
-
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server Listening on port ${process.env.PORT}`)
 })
